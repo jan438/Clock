a3bbea9dd6c69337ea02da2aa32835e3ccb34bd9
Jan Boon <janboon438@gmail.com>
2016-12-01 13:44:17 +0100

    Console logging extended with the zIndex

--- js/Clock.js
+++ js/Clock.js
@@ -228,7 +228,7 @@ var Deck = (function () {
 	var starttime = Date.now();
 	if (self.x === 0 && self.y === 0) {
 		if (clockcards[12].length > 0) {
-			console.log("Card13: " + $el.id);
+			console.log("Card13: " + $el.id + " z: " + $el.style.zIndex);
 			self.setSide("front");
 			var cardrank = self.rank;
 			if (cardrank === 12) cardrank = 0;
@@ -249,62 +249,62 @@ var Deck = (function () {
 	}
 	if (self.x === 0 && self.y === -360) {
 		if (clockcards[0].length > 0) {
-			console.log("Card0: " + $el.id);
+			console.log("Card0: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === 180 && self.y === -311) {
 		if (clockcards[1].length > 0) {
-			console.log("Card1: " + $el.id);
+			console.log("Card1: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === 311 && self.y === -180) {
 		if (clockcards[2].length > 0) {
-			console.log("Card2: " + $el.id);
+			console.log("Card2: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === 360 && self.y === 0) {
 		if (clockcards[3].length > 0) {
-			console.log("Card3: " + $el.id);
+			console.log("Card3: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === 311 && self.y === 180) {
 		if (clockcards[4].length > 0) {
-			console.log("Card4: " + $el.id);
+			console.log("Card4: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === 180 && self.y === 311) {
 		if (clockcards[5].length > 0) {
-			console.log("Card5: " + $el.id);
+			console.log("Card5: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === 0 && self.y === 360) {
 		if (clockcards[6].length > 0) {
-			console.log("Card6: " + $el.id);
+			console.log("Card6: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === -180 && self.y === 311) {
 		if (clockcards[7].length > 0) {
-			console.log("Card7: " + $el.id);
+			console.log("Card7: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === -311 && self.y === 180) {
 		if (clockcards[8].length > 0) {
-			console.log("Card8: " + $el.id);
+			console.log("Card8: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === -360 && self.y === 0) {
 		if (clockcards[9].length > 0) {
-			console.log("Card9: " + $el.id);
+			console.log("Card9: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === -311 && self.y === -180) {
 		if (clockcards[10].length > 0) {
-			console.log("Card10: " + $el.id);
+			console.log("Card10: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	if (self.x === -180 && self.y === -311) {
 		if (clockcards[11].length > 0) {
-			console.log("Card11: " + $el.id);
+			console.log("Card11: " + $el.id + " z: " + $el.style.zIndex);
 		}
 	}
 	e.preventDefault();
