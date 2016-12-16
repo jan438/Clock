66a484fef549ef6f3c2eaa08a880632deb3f9219
Jan Boon <janboon438@gmail.com>
2016-12-16 09:23:32 +0100

    All animations implemented step 3

--- js/Clock.js
+++ js/Clock.js
@@ -303,18 +303,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[12].splice(clockcards[12].length - 1, 1);
 					clockcards[12][clockcards[12].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -348,18 +337,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[0].splice(clockcards[0].length - 1, 1);
 					clockcards[0][clockcards[0].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -393,18 +371,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[1].splice(clockcards[1].length - 1, 1);
 					clockcards[1][clockcards[1].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -438,18 +405,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[2].splice(clockcards[2].length - 1, 1);
 					clockcards[2][clockcards[2].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -483,18 +439,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[3].splice(clockcards[3].length - 1, 1);
 					clockcards[3][clockcards[3].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -528,18 +473,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[4].splice(clockcards[4].length - 1, 1);
 					clockcards[4][clockcards[4].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -573,18 +507,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[5].splice(clockcards[5].length - 1, 1);
 					clockcards[5][clockcards[5].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -618,18 +541,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[6].splice(clockcards[6].length - 1, 1);
 					clockcards[6][clockcards[6].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -663,18 +575,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[7].splice(clockcards[7].length - 1, 1);
 					clockcards[7][clockcards[7].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -708,18 +609,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[8].splice(clockcards[8].length - 1, 1);
 					clockcards[8][clockcards[8].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -753,18 +643,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[9].splice(clockcards[9].length - 1, 1);
 					clockcards[9][clockcards[9].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -798,18 +677,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[10].splice(clockcards[10].length - 1, 1);
 					clockcards[10][clockcards[10].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
@@ -843,18 +711,7 @@ var Deck = (function () {
 					clockcards[cardrank].splice(0, 0, self);
 					clockcards[11].splice(clockcards[11].length - 1, 1);
 					clockcards[11][clockcards[11].length - 1].setSide("front");
-					self.animateTo({
-						delay: 250,
-						duration: 250,
-						x: cardposition[cardrank][0],
-						y: cardposition[cardrank][1],
-						rot: hourrotation * cardrank,
-						onStart: function onStart() {
-							$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
-						},
-						onComplete: function onComplete() {
-						}
-					});
+					$el.style.zIndex = clockcards[cardrank][1].$el.style.zIndex - 1;
 				}
 			}
 		}
